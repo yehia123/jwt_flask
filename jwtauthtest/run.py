4d12764b768803790cc8e0a135e7ec6923eb3b49
Yehia Qtaish <yehiaqtaish@gmail.com>
2020-07-29 11:22:39 -0700

    flask REST API calls for jwt authentication -- next to add frontend and fix logout feature

---
+++ jwtauthtest/run.py
@@ -0,0 +1,36 @@
+from flask import Flask
+from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager
+
+app = Flask(__name__)
+api = Api(app)
+db = SQLAlchemy(app)
+
+import resources, models, views
+
+api.add_resource(resources.UserRegistration, '/registration')
+api.add_resource(resources.UserLogin, '/login')
+api.add_resource(resources.UserLogoutAccess, '/logout/access')
+api.add_resource(resources.UserLogoutRefresh, '/logout/refresh')
+api.add_resource(resources.TokenRefresh, '/token/refresh')
+api.add_resource(resources.AllUsers, '/users')
+api.add_resource(resources.SecretResource, '/secret')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'ya-bun-tu'
+app.config['JWT_SECRET_KEY'] = 'jwt-secret-string'
+app.config['JWT_BLACKLIST_ENABLED']= True
+app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
+
+jwt = JWTManager(app)
+
+@app.before_first_request
+def create_table():
+    db.create_all()
+
+@jwt.token_in_blacklist_loader
+def check_if_token_in_blacklist(decrypted_token):
+    jti = decrypted_token['jti']
+    return models.RevokedTokenModel.is_jti_blacklisted(jti)
---
+++ jwtauthtest/views.py
@@ -0,0 +1,8 @@
+from run import app
+from flask import jsonify
+
+@app.route('/')
+def index():
+    return jsonify({'message': 'hello world!'})
+
+
